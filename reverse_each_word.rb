0e64a73173975b017c73bb4e8c8afb3c3d768761
Roni Shabo <roni.shabo@gmail.com>
2019-02-27 15:04:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_arr = []
-  string.collect do |string|
+  array.collect do |string|
   new_arr << string.reverse
   end
   new_arr.join(" ")
